2bbc399cadb4af61ae58952c16d4ec82d35b147e
C1383@oic.osaka.jp <C1383@oic.osaka.jp>
2023-10-10 14:21:52 +0900

    test

---
+++ GameSoftSaleSystems/FormSubscription.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace GameSoftSaleSystems
+{
+    partial class FormSubscription
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSubscription));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("HGP創英ﾌﾟﾚｾﾞﾝｽEB", 42.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(460, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 57);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "GSSS";
+            // 
+            // button3
+            // 
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(1282, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(33, 37);
+            this.button3.TabIndex = 38;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.textBox1.Location = new System.Drawing.Point(664, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(598, 31);
+            this.textBox1.TabIndex = 37;
+            this.textBox1.Text = "検索バー";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(470, 657);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(194, 80);
+            this.button7.TabIndex = 45;
+            this.button7.Text = "サブスクリプション";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(470, 569);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(194, 80);
+            this.button6.TabIndex = 44;
+            this.button6.Text = "プライバシー";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(470, 481);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(194, 80);
+            this.button5.TabIndex = 43;
+            this.button5.Text = "ログイン＆セキュリティ";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(470, 393);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(194, 80);
+            this.button4.TabIndex = 42;
+            this.button4.Text = "オーダー";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(470, 305);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 80);
+            this.button2.TabIndex = 41;
+            this.button2.Text = "ウォレット";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(470, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 80);
+            this.button1.TabIndex = 40;
+            this.button1.Text = "アカウント";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FormSubscription
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FormSubscription";
+            this.Text = "FormSubscription";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
