7aabb2278476f86adcea86b92a13f2d6e19d5a57
nagai <c1248@oic.jp>
2023-10-10 14:56:46 +0900

    開発用画面移動画面の作成

---
+++ GameSoftSaleSystems/開発用画面移動画面.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameSoftSaleSystems
+{
+    public partial class 開発用画面移動画面 : Form
+    {
+        public 開発用画面移動画面()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void OpenForm(string formName)
+        {
+            Form frm = new Form();
+            //引数より、開くフォームを設定
+            switch (formName)
+            {
+                case "FormMainmenu":
+                    frm = new FormMainmenu();
+                    break;
+            }
+
+            //フォームを透明化
+            Opacity = 0;
+
+            //選択されたフォームを開く
+            frm.ShowDialog();
+
+            //開いたフォームから戻ってきたらメモリを解放する
+            frm.Dispose();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenForm("FormMainmenu");
+        }
+    }
+}
